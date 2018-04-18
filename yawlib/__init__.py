218d7575784d050b6b826423b5e88f06a0f65fd5
Le Tuan Anh <tuananh.ke@gmail.com>
2018-04-18 12:00:30 +0800

    Use AppConfig to config Yawlib. Fix setup script

--- yawlib/__init__.py
+++ yawlib/__init__.py
@@ -3,41 +3,16 @@
 
 '''
 YAWLib - Yet another WordNet library for Python
+
 Latest version can be found at https://github.com/letuananh/yawlib
 
-Adapted from: https://github.com/letuananh/lelesk
-
-@author: Le Tuan Anh <tuananh.ke@gmail.com>
+:copyright: (c) 2012 Le Tuan Anh <tuananh.ke@gmail.com>
+:license: MIT, see LICENSE for more details.
 '''
 
-# Copyright (c) 2016, Le Tuan Anh <tuananh.ke@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-__author__ = "Le Tuan Anh <tuananh.ke@gmail.com>"
-__copyright__ = "Copyright 2014, yawlib"
-__credits__ = []
-__license__ = "MIT"
-__version__ = "0.1"
-__maintainer__ = "Le Tuan Anh"
-__email__ = "<tuananh.ke@gmail.com>"
-__status__ = "Prototype"
+from .__version__ import __author__, __email__, __copyright__, __maintainer__
+from .__version__ import __credits__, __license__, __description__, __url__
+from .__version__ import __version_major__, __version_long__, __version__, __status__
 
 from .config import YLConfig
 from .models import SynsetID, POS, Synset, SynsetCollection
@@ -51,4 +26,5 @@ __all__ = ['YLConfig', 'GWordnetXML', 'GWordnetSQLite', 'WordnetSQL',
            'POS', 'SynsetID', 'Synset', 'SynsetCollection',
            'get_synset_by_id', 'get_synset_by_sk',
            'get_synsets_by_term', 'dump_synsets', 'dump_synset',
-           'WordnetException', 'SynsetNotFoundException']
+           'WordnetException', 'SynsetNotFoundException',
+           "__version__", "__author__", "__description__", "__copyright__"]
